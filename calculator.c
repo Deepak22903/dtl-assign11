8d61fcdf4634ae444865688e006506cb7935ce0d
Deepak22903 <126399692+Deepak22903@users.noreply.github.com>
2023-12-12 14:40:40 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -16,9 +16,6 @@ int main() {
         case '+':
             result = num1 + num2;
             break;
-        case '-':
-            result = num1 - num2;
-            break;
         case '*':
             result = num1 * num2;
             break;
